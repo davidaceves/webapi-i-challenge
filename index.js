9d2efc0a8865ab40aa1eb8dd9e3f1419843394d2
David <digital0david@gmail.com>
2019-05-08 23:25:40 -0400

    post and get

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // implement your API here
+
+const express = require('express');
+
+const db = require('./data/db.js')
+const server = express();
+server.use(express.json());
+
+server.listen(9090, () => {
+    console.log('Listening on port 9090')
+});
+
+server.post('/api/users', (req, res) => {
+    const { name, bio } = req.body;
+
+    if (!name || !bio) {
+        res.status(400).json({ errorMessage: "Must provide name and bio" });
+        return; 
+    }
+
+    db.insert({
+        name,
+        bio, 
+    })
+    .then(() => {
+        res.status(201).json({ name, bio })
+    })
+    .catch(() => {
+        res.status(500).json({ error: "There was an error saving the user to the database" })
+        return;
+    })   
+})
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        res.status(201).json(users)
+    })
+    .catch(() => {
+        res.status(500).json({ error: "The users information could not be retrieved." })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    db.findById()
+})
