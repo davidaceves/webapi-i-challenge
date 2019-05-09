b539b416ff1aeafda0344d88a943d976bb995781
David <digital0david@gmail.com>
2019-05-09 00:30:14 -0400

    put

--- index.js
+++ index.js
@@ -72,3 +72,26 @@ server.delete('/api/users/:id', (req, res) => {
         res.status(500).json({ error: "The user could not be removed" })
     })
 })
+
+server.put('/api/users/:id', (req, res) => {
+    // const { name, bio } = req.params;
+    const { id, name, bio } = req.body;
+    const changes = req.body;
+
+    if (!name || !bio) {
+        res.status(400).json({ errorMessage: "Must provide name and bio for the user" });
+        return; 
+    }
+
+    db.update(id, changes)
+    .then(updateUser => {
+        if (updateUser) {
+            res.status(200).json({ name, bio})
+        } else {
+            res.status(404).json({ message: "The user with the specified ID does not exist"})
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ error: "The user information could not be modified." })
+    })
+})
